6aceb89ebbb7fc1b2711ee54386b97f01aa7fdf6
SinclairPythonAkoto <sinclair.python@gmail.com>
2019-08-16 14:58:12 +0100

    schema changed

--- schema.sql
+++ schema.sql
@@ -1,12 +1,12 @@
 DROP TABLE if exists jobslog_01;
 CREATE TABLE jobslog_01 (
   id SERIAL PRIMARY KEY,
-  date DATE,
-  time TIME,
+  date VARCHAR,
+  time VARCHAR,
   job VARCHAR,
   description VARCHAR,
   outcome VARCHAR,
   comments VARCHAR,
-  date_stamp DATE,
-  time_stamp TIME
+  date_stamp VARCHAR,
+  time_stamp VARCHAR
   );
